1b9ee2b1ad3111b8298ad069771ad12b2be8b2d4
Brad Gessler <bradgessler@gmail.com>
2023-06-27 14:41:44 -0700

    Add base classes for Superform

--- lib/superform.rb
+++ lib/superform.rb
@@ -4,5 +4,161 @@ require_relative "superform/version"
 
 module Superform
   class Error < StandardError; end
-  # Your code goes here...
+
+  class DOM
+    def initialize(field)
+      @field = field
+    end
+
+    def value
+      @field.value
+    end
+
+    def id
+      lineage.map(&:id).join("_")
+    end
+
+    def name
+      root, *names = lineage.map(&:name)
+      names.map { |name| "[#{name}]" }.unshift(root).join
+    end
+
+    def inspect
+      "<id=#{id.inspect} name=#{name.inspect} value=#{value.inspect}/>"
+    end
+
+    def lineage
+      Enumerator.produce(@field, &:parent).take_while(&:itself).reverse
+    end
+  end
+
+  class Field
+    attr_reader :id, :name, :value, :parent, :children
+    attr_writer :value
+
+    def initialize(id, name: nil, value: nil, parent:, builder:)
+      @id = id
+      @name = name || id
+      @value = value
+      @parent = parent
+      @builder = builder
+      @children = []
+      yield self if block_given?
+    end
+
+    def field(id, value: nil, **kwargs, &)
+      value ||= @builder.get(id)
+      self.class.new(id, value: value, parent: self, builder: @builder, **kwargs, &).tap do |child|
+        @children.append child
+      end
+    end
+
+    def each(&)
+      Array(@value).each.with_index do |value, index|
+        self.class.new(index, name: :"", value: value, parent: self, builder: @builder, &).tap do |child|
+          @children.append child
+        end
+      end
+    end
+
+    def dom
+      DOM.new(self)
+    end
+
+    def self.root(*args, **kwargs, &block)
+      new *args, parent: nil, **kwargs, &block
+    end
+  end
+
+  class Mapper
+    def attributes(field)
+      if field.children.any?
+        { field.id => field.children.map { |child| attributes(child) }.reduce(:merge) }
+      else
+        { field.id => field.value }
+      end
+    end
+  end
+
+  class ParametersMapper < Mapper
+    attr_reader :params
+
+    def initialize(params)
+      @params = params
+    end
+
+    def assign(field)
+      field.children.each do |child|
+        if value = params.fetch(child.id, nil)
+          if child.children.any?
+            assign(child)
+          else
+            child.value = value
+          end
+        end
+      end
+    end
+
+    def permit(field)
+      permitted_attributes = field.children.map(&:id)
+      params.select { |k, _| permitted_attributes.include?(k) }
+    end
+  end
+
+  class ObjectMapper < Mapper
+    attr_reader :object
+
+    def initialize(object)
+      @object = object
+    end
+
+    def assign(field)
+      field.children.each do |child|
+        if object.respond_to?(child.id)
+          value = object.send(child.id)
+          if child.children.any?
+            assign(child)
+          else
+            child.value = value
+          end
+        end
+      end
+    end
+  end
+
+  class Builder
+    attr_reader :object
+
+    def initialize(object)
+      @object = object
+    end
+  end
+
+  class ObjectBuilder < Builder
+    def get(key)
+      @object.send key if key? key
+    end
+
+    def set(key, value)
+      @object.send "#{key}=", value if @object.respond_to? "#{key}="
+    end
+
+    def key?(key)
+      @object.respond_to? key
+    end
+  end
+
+  class HashBuilder < Builder
+    def get(key)
+      @object.fetch key if key? key
+    end
+
+    def set(key, value)
+      @object[key] = value
+    end
+
+    def key?(key)
+      @object.key? key
+    end
+  end
 end
--- spec/superform_spec.rb
+++ spec/superform_spec.rb
@@ -1,11 +1,56 @@
 # frozen_string_literal: true
 
+User = Data.define(:name, :nicknames, :addresses)
+Address = Data.define(:street, :city, :state)
+
 RSpec.describe Superform do
-  it "has a version number" do
-    expect(Superform::VERSION).not_to be nil
+  let(:user) do
+    User.new \
+      name: "William",
+      nicknames: ["Bill", "Billy", "Will"],
+      addresses: [
+        Address.new(street: "Birch Ave", city: "Williamsburg", state: "New Mexico"),
+        { street: "Main St", city: "Salem", state: "Indiana"},
+      ]
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:params) do
+    {
+      name: "Brad",
+      admin: true,
+      nicknames: ["Billy", "Swanson"],
+      addresses: [
+        { street: "Main St", city: "Salem", state: "Indiana"},
+        { street: "Wall St", city: "New York", state: "New York", admin: true }
+      ],
+      one: { two: { three: { four: 100 }}}
+    }
+  end
+
+  let(:form) do
+    Superform::Field.root :user, builder: Superform::ObjectBuilder.new(user) do |form|
+      form.field(:name)
+      form.field(:nicknames).each do |field|
+        pp field.dom
+      end
+      form.field(:addresses).each do |address|
+        address.field(:street)
+        address.field(:city)
+        address.field(:state)
+      end
+      form.field(:one).field(:two).field(:three).field(:four, value: 100).dom
+    end
+  end
+
+  it "permits params only in form" do
+    expect(Superform::ParametersMapper.new(params).permit(form)).to eql({
+      name: "Brad",
+      nicknames: ["Billy", "Swanson"],
+      addresses: [
+        {street: "Main St", city: "Salem", state: "Indiana"},
+        {street: "Wall St", city: "New York", state: "New York"}
+      ],
+      one: {two: {three: {four: 100}}}
+    })
   end
 end
