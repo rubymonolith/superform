d6a4de385454004394199af9f64324a255b0e6de
Brad Gessler <bradgessler@gmail.com>
2023-06-28 23:28:55 -0700

    Specs for the Field builder

--- spec/superform_spec.rb
+++ spec/superform_spec.rb
@@ -65,4 +65,28 @@ RSpec.describe Superform do
       one: {two: {three: {four: 100}}}
     })
   end
+
+  it "has correct DOM names" do
+    Superform::Field.root :user, value: user do |form|
+      form.field(:name).dom.tap do |dom|
+        expect(dom.id).to eql("user_name")
+        expect(dom.name).to eql("user[name]")
+      end
+      form.field(:nicknames).each do |field|
+        field.dom.tap do |dom|
+          expect(dom.id).to match /user_nicknames_\d+/
+          expect(dom.name).to eql("user[nicknames][]")
+        end
+      end
+      form.field(:addresses).each do |address|
+        address.field(:street).dom.tap do |dom|
+          expect(dom.id).to match /user_addresses_\d_street+/
+          expect(dom.name).to eql("user[addresses][][street]")
+        end
+        address.field(:city)
+        address.field(:state)
+      end
+      form.field(:one).field(:two).field(:three).field(:four, value: 100).dom
+    end
+  end
 end
