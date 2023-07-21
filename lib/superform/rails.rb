8551b72d31790d57620da7d4753eefc39b25606e
Brad Gessler <bradgessler@gmail.com>
2023-07-21 01:19:51 -0700

    Add Superform::Rails::StrongParameters
    This is really a cheesy way to do it, but its the most compatible with vanilla Rails applications that use instance variables like its 1999.

--- lib/superform/rails.rb
+++ lib/superform/rails.rb
@@ -100,6 +100,24 @@ module Superform
       end
     end
 
+    module StrongParameters
+      protected
+        # Assigns params to the form model.
+        def assign(params, to:, form: form_class)
+          to.tap do |model|
+            form.new(model).tap do |form|
+              # TODO: Figure out how to render this in a way that doesn't concat a string; just throw everything away.
+              render_to_string form
+              form.assign params
+            end
+          end
+        end
+
+        # Defaults to the form defined in `./app/views/*/form.rb`.
+        def form_class
+          self.controller_name.camelize.constantize::Form
+        end
+    end
 
     module Components
       class FieldComponent < ApplicationComponent
