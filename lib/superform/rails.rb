ef249ee872e3bc0286ab0c7fa37e80a0957f3cd1
Brad Gessler <bradgessler@gmail.com>
2023-07-22 11:21:31 -0700

    Remove form class constructor from assigns

--- lib/superform/rails.rb
+++ lib/superform/rails.rb
@@ -102,20 +102,13 @@ module Superform
 
     module StrongParameters
       protected
-        # Assigns params to the form model.
-        def assign(params, to:, form: form_class)
-          to.tap do |model|
-            form.new(model).tap do |form|
-              # TODO: Figure out how to render this in a way that doesn't concat a string; just throw everything away.
-              render_to_string form
-              form.assign params
-            end
-          end
-        end
-
-        # Defaults to the form defined in `./app/views/*/form.rb`.
-        def form_class
-          self.controller_name.camelize.constantize::Form
+        # Assigns params to the form and returns the model.
+        def assign(params, to:)
+          form = to
+          # TODO: Figure out how to render this in a way that doesn't concat a string; just throw everything away.
+          render_to_string form
+          form.assign params
+          form.model
         end
     end
 
