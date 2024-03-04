35697ed99b428c49fd0d3da37377d46a7495ce2d
Brad Gessler <bradgessler@gmail.com>
2024-03-04 15:00:40 -0800

    Detect of phlex-rails is installed before trying to install it

--- lib/generators/superform/install/install_generator.rb
+++ lib/generators/superform/install/install_generator.rb
@@ -1,9 +1,13 @@
+require "bundler"
+
 class Superform::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path("templates", __dir__)
 
   APPLICATION_CONFIGURATION_PATH = Rails.root.join("config/application.rb")
 
   def install_phlex_rails
+    return if gem_in_bundle? "phlex-rails"
+
     gem "phlex-rails"
     generate "phlex:install"
   end
@@ -21,4 +25,9 @@ class Superform::InstallGenerator < Rails::Generators::Base
   def create_application_form
     template "application_form.rb", Rails.root.join("app/views/forms/application_form.rb")
   end
+
+  private
+    def gem_in_bundle?(gem_name)
+      Bundler.load.specs.any? { |spec| spec.name == gem_name }
+    end
 end
