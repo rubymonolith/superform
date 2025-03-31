943eae2be607c2dcf1fbd6d946ca83833ff3719f
Marco van Kampen <marco@winkwaves.com>
2025-03-31 15:57:56 +0200

    Add specs for NamespaceCollection#each
    [#43]

--- spec/superform/namespace_collection_spec.rb
+++ spec/superform/namespace_collection_spec.rb
@@ -1,18 +1,31 @@
 RSpec.describe Superform::NamespaceCollection do
+  subject(:collection) do
+    described_class.new(:bars, parent: namespace) do |collection|
+      collection.field(:baz)
+    end
+  end
+
+  let(:namespace) { Superform::Namespace.new(:foo, parent: nil, object: object) }
+  let(:object) do
+    OpenStruct.new(
+      bars: [
+        OpenStruct.new(baz: "A"),
+        OpenStruct.new(baz: "B")
+      ]
+    )
+  end
+
+  describe "each" do
+    it "yields a namespace for each collection item" do
+      expect { |b| collection.each(&b) }.to yield_successive_args(
+        an_object_satisfying { |item| item.is_a?(Superform::Namespace) && item.key == 0 },
+        an_object_satisfying { |item| item.is_a?(Superform::Namespace) && item.key == 1 }
+      )
+    end
+  end
+
   describe "#assign" do
     it "assigns the value to each namespace" do
-      object = OpenStruct.new(
-        bars: [
-          OpenStruct.new(baz: "A"),
-          OpenStruct.new(baz: "B")
-        ]
-      )
-
-      namespace = Superform::Namespace.new(:foo, parent: nil, object:)
-      collection = described_class.new(:bars, parent: namespace) do |collection|
-        collection.field(:baz)
-      end
-
       collection.assign([{ baz: "C" },{ baz: "D" }])
       expect(collection.serialize).to eq([{ baz: "C" },{ baz: "D" }])
     end
