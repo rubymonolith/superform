166f20821100e3627a68254235df0d692beed85c
Brad Gessler <bradgessler@gmail.com>
2025-08-20 10:36:48 -0700

    Detect the Phlex superclass to extend

--- lib/superform/rails.rb
+++ lib/superform/rails.rb
@@ -5,7 +5,19 @@ module Superform
     # When this is included in a Rails app, check if `Components::Base` is defined and
     # inherit from that so we get all the users stuff; otherwise inherit from Phlex::HTML,
     # which means we won't have all the users methods and overrides.
-    Component = defined?(::Components::Base) ? ::Components::Base : ::Phlex::HTML
+    SUPERCLASSES = [
+      "::Components::Base",       # Phlex 2.x base class in a Rails project
+      "::ApplicationComponent",   # Phlex 1.x base class in a Rails project
+      "::Phlex::HTML",            # Couldn't detect a base Phlex Rails class, so use Phlex::HTML
+    ]
+
+    # Find the base class for the Rails app.
+    def self.base_class
+      const_get SUPERCLASSES.find { |const| const_defined?(const) }
+    end
+
+    # Set the base class for the remainder of this library.
+    Component = base_class
 
     # A Phlex::HTML view module that accepts a model and sets a `Superform::Namespace`
     # with the `Object#model_name` as the key and maps the object to form fields
