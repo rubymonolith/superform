95545e800cc557304d8e213ac1d443e57d3ba7a1
Brad Gessler <bradgessler@gmail.com>
2025-08-22 17:38:40 -0700

    Fix label component

---
+++ lib/superform/rails/components/label.rb
@@ -0,0 +1,16 @@
+module Superform
+  module Rails
+    module Components
+      class Label < Base
+        def view_template(&content)
+          content ||= Proc.new { field.key.to_s.titleize }
+          label(**attributes, &content)
+        end
+
+        def field_attributes
+          { for: dom.id }
+        end
+      end
+    end
+  end
+end
