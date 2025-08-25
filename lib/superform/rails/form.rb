6cf3b163b6c089bfd9bc9f39c84721b3d1b32352
Brad Gessler <bradgessler@gmail.com>
2025-08-25 11:00:07 -0700

    Add convenience methods for form fields, e.g. `input(type: :email)` is now `email`

--- lib/superform/rails/form.rb
+++ lib/superform/rails/form.rb
@@ -54,6 +54,10 @@ module Superform
           Components::Input.new(self, attributes:)
         end
 
+        def text(*, **, &)
+          input(*, **, type: :text, &)
+        end
+
         def checkbox(**attributes)
           Components::Checkbox.new(self, attributes:)
         end
@@ -69,6 +73,82 @@ module Superform
         def select(*collection, **attributes, &)
           Components::Select.new(self, attributes:, collection:, &)
         end
+
+        # HTML5 input type convenience methods - clean API without _field suffix
+        # Examples:
+        #   field(:email).email(class: "form-input")
+        #   field(:age).number(min: 18, max: 99)
+        #   field(:birthday).date
+        #   field(:secret).hidden(value: "token123")
+        #   field(:gender).radio("male", id: "user_gender_male")
+        def hidden(*, **, &)
+          input(*, **, type: :hidden, &)
+        end
+
+        def password(*, **, &)
+          input(*, **, type: :password, &)
+        end
+
+        def email(*, **, &)
+          input(*, **, type: :email, &)
+        end
+
+        def url(*, **, &)
+          input(*, **, type: :url, &)
+        end
+
+        def tel(*, **, &)
+          input(*, **, type: :tel, &)
+        end
+        alias_method :phone, :tel
+
+        def number(*, **, &)
+          input(*, **, type: :number, &)
+        end
+
+        def range(*, **, &)
+          input(*, **, type: :range, &)
+        end
+
+        def date(*, **, &)
+          input(*, **, type: :date, &)
+        end
+
+        def time(*, **, &)
+          input(*, **, type: :time, &)
+        end
+
+        def datetime(*, **, &)
+          input(*, **, type: :"datetime-local", &)
+        end
+
+        def month(*, **, &)
+          input(*, **, type: :month, &)
+        end
+
+        def week(*, **, &)
+          input(*, **, type: :week, &)
+        end
+
+        def color(*, **, &)
+          input(*, **, type: :color, &)
+        end
+
+        def search(*, **, &)
+          input(*, **, type: :search, &)
+        end
+
+        def file(*, **, &)
+          input(*, **, type: :file, &)
+        end
+
+        def radio(value, *, **, &)
+          input(*, **, type: :radio, value: value, &)
+        end
+
+        # Rails compatibility aliases
+        alias_method :check_box, :checkbox
+        alias_method :text_area, :textarea
 
         def title
           key.to_s.titleize
---
+++ spec/superform/rails/field_convenience_methods_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Superform::Rails::Form::Field do
+  let(:user) { User.new(email: "test@example.com", first_name: "John") }
+  let(:form) { Superform::Rails::Form.new(user) }
+  let(:field) { form.field(:email) }
+
+  describe "HTML5 input convenience methods" do
+    it { expect(field.text.type).to eq("text") }
+    it { expect(field.hidden.type).to eq("hidden") }
+    it { expect(field.password.type).to eq("password") }
+    it { expect(field.email.type).to eq("email") }
+    it { expect(field.url.type).to eq("url") }
+    it { expect(field.tel.type).to eq("tel") }
+    it { expect(field.phone.type).to eq("tel") }
+    it { expect(field.number.type).to eq("number") }
+    it { expect(field.range.type).to eq("range") }
+    it { expect(field.date.type).to eq("date") }
+    it { expect(field.time.type).to eq("time") }
+    it { expect(field.datetime.type).to eq("datetime-local") }
+    it { expect(field.month.type).to eq("month") }
+    it { expect(field.week.type).to eq("week") }
+    it { expect(field.color.type).to eq("color") }
+    it { expect(field.search.type).to eq("search") }
+    it { expect(field.file.type).to eq("file") }
+    it { expect(field.radio("male").type).to eq("radio") }
+  end
+
+  describe "Rails compatibility aliases" do
+    describe "#check_box" do
+      it "creates checkbox component" do
+        component = field.check_box
+        expect(component).to be_a(Superform::Rails::Components::Checkbox)
+      end
+    end
+
+    describe "#text_area" do
+      it "creates textarea component" do
+        component = field.text_area
+        expect(component).to be_a(Superform::Rails::Components::Textarea)
+      end
+    end
+  end
+
+  describe "argument forwarding" do
+    it "passes through all attributes correctly" do
+      component = field.email(class: "form-input", required: true, placeholder: "Enter email")
+      expect(component.type).to eq("email")
+    end
+
+    it "allows type override with input method" do
+      component = field.input(type: :search, class: "form-input")
+      expect(component.type).to eq("search")
+    end
+
+    it "handles radio button value parameter correctly" do
+      component = field.radio("female", class: "radio-input", data: { value: "f" })
+      expect(component.type).to eq("radio")
+    end
+  end
+end
