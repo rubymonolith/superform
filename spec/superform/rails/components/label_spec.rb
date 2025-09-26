7e0d4138728ce47dec4a261755823dfbfed56bfb
Brad Gessler <bradgessler@gmail.com>
2025-09-25 21:53:24 -0700

    Fix label spec

--- spec/superform/rails/components/label_spec.rb
+++ spec/superform/rails/components/label_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe Superform::Rails::Components::Label do
   let(:form) { Superform::Rails::Form.new(user) }
   let(:field) { form.field(:first_name) }
   let(:label) { described_class.new(field, attributes: { class: "form-label" }) }
-  
+
   subject { label.call }
 
   it "renders label with titleized field name" do
-    expect(subject).to eq('<label for="user_first_name" class="form-label">First Name</label>')
+    expect(subject).to eq('<label for="user_first_name" class="form-label">First name</label>')
   end
 end
